72bcc2d2b7517db1201ce2bfdf4c72fe4ff2a5c5
dbrajesh007 <dbrajesh007@gmail.com>
2019-06-21 15:29:17 +0530

    api commit

---
+++ Api.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/price')
+def HelloWorld():
+    import pyodbc
+
+
+    server = 'sunthedb.database.windows.net'
+    database = 'sunthemart'
+    username = 'suntheadmin'
+    password = '$unthe123$'
+    
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ',1433', user=username, password=password, database=database)
+    cursor = conn.cursor()
+                               
+
+    cur = conn.cursor()
+
+    cur.execute("select price from sm_Price where product_id = 1")
+    
+    for i in cur:
+        
+        return str(i)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0',port = 5000)
