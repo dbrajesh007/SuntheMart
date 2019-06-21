372125a50923649cc98dfe03a178cd89c4bce2a8
dbrajesh007 <dbrajesh007@gmail.com>
2019-06-21 16:16:20 +0530

    update api

--- Api.py
+++ Api.py
@@ -4,27 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/price')
-def HelloWorld():
-    import pyodbc
-
-
-    server = 'sunthedb.database.windows.net'
-    database = 'sunthemart'
-    username = 'suntheadmin'
-    password = '$unthe123$'
+def Hello():
+    return "hello_world"
     
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ',1433', user=username, password=password, database=database)
-    cursor = conn.cursor()
-                               
-
-    cur = conn.cursor()
-
-    cur.execute("select price from sm_Price where product_id = 1")
-    
-    for i in cur:
-        
-        return str(i)
 
 if __name__ == '__main__':
-    
     app.run()
